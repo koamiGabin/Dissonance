989831daa71377e614c176651c63293d5a4b79db
Axel <axel.poidebard@gmail.com>
2025-10-08 16:57:24 +0200

    lobby delete

--- frontend/app/components/navbar.vue
+++ frontend/app/components/navbar.vue
@@ -7,7 +7,6 @@
             <ul class="menu menu-horizontal px-1">
                 <li><NuxtLink to="/jouer" class="btn btn-ghost text-l">Le Jeu</NuxtLink></li>
                 <li><NuxtLink to="/regles" class="btn btn-ghost text-l">Les Règles</NuxtLink></li>
-                <li><NuxtLink to="/lobby" class="btn btn-ghost text-l">Lobby</NuxtLink></li>
                 <li><NuxtLink to="/indices" class="btn btn-ghost text-l">Les Indices</NuxtLink></li>              
             </ul>
         </div>
